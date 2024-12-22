fc2188297c783396c9bccb2323d5da28f675e943
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-12-22 07:29:38 +0530

    initial commit with project tailwind setup and dep installed

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-5xl bg-clip-text bg-gradient-to-r from-blue-500 to-pink-600 font-bold text-transparent text-center mt-5">
+      Todo App With Redux Toolkit
+    </h1>
+  );
+}
+
+export default App;
