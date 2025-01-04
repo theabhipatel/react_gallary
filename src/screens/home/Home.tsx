50d9f7f20cbdaad37ff2a008a803ddc684e7461c
TheAbhiPatel <theabhipatel.co@gmail.com>
2025-01-04 17:25:21 +0530

    design home page for react gallary

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -2,48 +2,58 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-5xl bg-clip-text bg-gradient-to-r from-blue-500 to-pink-600 font-bold text-transparent text-center my-5">
-        React Gallary
-      </h1>
-      <div className="flex justify-center">
-        <div className="w-[90%] md:w-[60%] p-5 rounded-md flex flex-wrap justify-center items-center gap-2 mt-2 bg-indigo-600/10">
-          <Link to={"/todo"}>
-            <button className="bg-indigo-500/90 h-8 w-32 flex justify-center items-center rounded-md text-white">
-              Todo App
-            </button>
-          </Link>
-          <Link to={"/dynamic-form"}>
-            <button className="bg-indigo-500/90 h-8 w-32 flex justify-center items-center rounded-md text-white">
-              Dynamic Form
-            </button>
-          </Link>
-          <Link to={"/accordion"}>
-            <button className="bg-indigo-500/90 h-8 w-32 flex justify-center items-center rounded-md text-white">
-              Accordian
-            </button>
-          </Link>
-          <Link to={"/infinite-scroll"}>
-            <button className="bg-indigo-500/90 h-8 w-32 flex justify-center items-center rounded-md text-white">
-              Infinite Scroll
-            </button>
-          </Link>
-          <Link to={"/drag-and-drop"}>
-            <button className="bg-indigo-500/90 h-8 w-32 flex justify-center items-center rounded-md text-white">
-              Dran and Drop
-            </button>
-          </Link>
-          <Link to={"/no-internet"}>
-            <button className="bg-indigo-500/90 h-8 w-32 flex justify-center items-center rounded-md text-white">
-              No Internet
-            </button>
-          </Link>
-          <Link to={"/progress"}>
-            <button className="bg-indigo-500/90 h-8 w-32 flex justify-center items-center rounded-md text-white">
-              Progress bar
-            </button>
-          </Link>
+    <div className="h-screen flex flex-col justify-around bg-gradient-to-tl from-[#09071d] to-[#021149]">
+      <div>
+        <h1 className="text-5xl bg-clip-text bg-gradient-to-r from-blue-600  to-pink-700 font-bold text-transparent backdrop-blur-md text-center py-5">
+          React Gallary
+        </h1>
+
+        <div className="flex justify-center">
+          <div className="w-[90%] md:w-[60%] p-5 rounded-md flex flex-wrap justify-center items-center gap-2 mt-2 bg-indigo-500/10 backdrop-blur-md border border-indigo-100/10 shadow-lg">
+            <Link to={"/todo"}>
+              <button className="bg-indigo-500/20 hover:bg-indigo-500/40 duration-300 backdrop-blur-lg border border-indigo-100/20 shadow-lg h-8 w-32 flex justify-center items-center rounded-md text-white">
+                Todo App
+              </button>
+            </Link>
+            <Link to={"/dynamic-form"}>
+              <button className="bg-indigo-500/20 backdrop-blur-lg border border-indigo-100/20 shadow-lg h-8 w-32 flex justify-center items-center rounded-md text-white hover:bg-indigo-500/40 duration-300">
+                Dynamic Form
+              </button>
+            </Link>
+            <Link to={"/accordion"}>
+              <button className="bg-indigo-500/20 backdrop-blur-lg border border-indigo-100/20 shadow-lg h-8 w-32 flex justify-center items-center rounded-md text-white hover:bg-indigo-500/40 duration-300">
+                Accordian
+              </button>
+            </Link>
+            <Link to={"/infinite-scroll"}>
+              <button className="bg-indigo-500/20 backdrop-blur-lg border border-indigo-100/20 shadow-lg h-8 w-32 flex justify-center items-center rounded-md text-white hover:bg-indigo-500/40 duration-300">
+                Infinite Scroll
+              </button>
+            </Link>
+            <Link to={"/drag-and-drop"}>
+              <button className="bg-indigo-500/20 backdrop-blur-lg border border-indigo-100/20 shadow-lg h-8 w-32 flex justify-center items-center rounded-md text-white hover:bg-indigo-500/40 duration-300">
+                Dran and Drop
+              </button>
+            </Link>
+            <Link to={"/no-internet"}>
+              <button className="bg-indigo-500/20 backdrop-blur-lg border border-indigo-100/20 shadow-lg h-8 w-32 flex justify-center items-center rounded-md text-white hover:bg-indigo-500/40 duration-300">
+                No Internet
+              </button>
+            </Link>
+            <Link to={"/progress"}>
+              <button className="bg-indigo-500/20 backdrop-blur-lg border border-indigo-100/20 shadow-lg h-8 w-32 flex justify-center items-center rounded-md text-white hover:bg-indigo-500/40 duration-300">
+                Progress bar
+              </button>
+            </Link>
+          </div>
         </div>
+      </div>
+
+      {/* ---> TheAbhi Patel Logo */}
+      <div className=" text-center">
+        <h1 className="bg-gradient-to-r from-orange-700 via-orange-500 to-yellow-500 bg-clip-text text-5xl font-bold text-transparent sm:text-8xl">
+          TheAbhiPatel
+        </h1>
       </div>
     </div>
   );
